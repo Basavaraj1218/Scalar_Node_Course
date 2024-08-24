0e89999be357bc3d12b6ba0adf8f77d04ffb6723
Bassu <basavarajstalwar2@gmail.com>
2024-08-24 12:02:05 +0530

    hello3

--- 4_MongoDB/index.js
+++ 4_MongoDB/index.js
@@ -7,10 +7,17 @@ mongoose.connect('mongodb://127.0.0.1/testDatabase')
 //schema
 
 const CourseSchema = new mongoose.Schema({
-  name : String,
-  creator : String,
+  name : {type : String, required:true, minlegth:5, maxlength:200},
+  tags : {type: Array, validate : {
+    validator : function (tags) {
+      return tags.length >1
+    }
+  }},
+  category : {type:String, required:true, enum : ['DSA', 'Web']},
+  creator : {type :String, required:true},
   publishedDate : {type:Date, default:Date.now},
-  isPublished : Boolean 
+  isPublished : Boolean ,
+  rating : {type: Number, required:function () {return this.isPublished}}
 })
 
 
@@ -18,19 +25,31 @@ const Course = mongoose.model('course', CourseSchema)
 
 async function createCourse() {
   const course = new Course({
-    name : 'Java',
-    creator :'abd',
-    isPublished : false
+    name : 'mango',
+    tags : ['express' ,'mangodb'],
+    category:'Web',
+    creator:'minal',
+    isPublished:true,
+    rating: 4.5
   })
+  
+  try {
+    const results = await course.save()
+    console.log(results)
+  }catch(error){
+    for(field in error.errors) {
+      console.log(error.errors[field])
+    }
+  }
 
-  const results = await course.save()
   //console.log(results)
 }
 createCourse()
-
+/*
 async function getCourses() {
   const courses = await Course.find({creator:'minal'}).select({name:1})
   console.log(courses)
 }
 getCourses()
+*/
 
